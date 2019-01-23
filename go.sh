c09fe4ecce4faf8031f0c898bb4277c3db444b6d
Eaglewzw <1460853569@qq.com>
2019-01-23 16:52:45 +0800

    MYTest

--- go.sh
+++ go.sh
@@ -1,4 +1,8 @@
-git config user.name "Eaglewzw"
-git config user.email "1460853569@qq.com"
+git init
 git add .
+git remote add origin https://github.com/Eaglewzw/GRBLApplication.git
+git config user.name "Eaglewzw"
+git config user.password "12345wzw"
+git pull origin master
 git push -u origin master
+
