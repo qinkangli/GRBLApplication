287d17910a5612c4e5f0c2b4423f7ba54e6d5757
Eaglewzw <1460853569@qq.com>
2019-01-29 22:06:31 +0800

    new idea

--- GRBL_APP/GRBLApplication/app/src/main/java/com/example/grblapplication/DeviceListActivity.java
+++ GRBL_APP/GRBLApplication/app/src/main/java/com/example/grblapplication/DeviceListActivity.java
@@ -22,112 +22,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import java.util.Set;
+import java.util.List;
 import java.util.ArrayList;
 
 
 public class DeviceListActivity extends Activity {
 
-
-
-    //本地蓝牙适配器
-    private BluetoothAdapter mBluetoothAdapter;
-    //用来存放搜到的蓝牙
-    private Set<BluetoothDevice> mDevices;
-    private ListView mListView;
-    private ArrayList mList;
-    private ArrayAdapter mArrayAdapter;
-    private TextView mConnectedView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
 
 
-
-        Button CancelButton = (Button) findViewById(R.id.button_cancel);
-        Button ScanButton= (Button) findViewById(R.id.button_scan);
-
-        mListView = (ListView) findViewById(R.id.paired_devices);
-
-
-        //获取蓝牙适配器
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        IntentFilter filter=new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver,filter);
-        IntentFilter filter2=new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        registerReceiver(mReceiver,filter2);
-
-
-
-
-        // 判断设备是否支持蓝牙功能
-        if (mBluetoothAdapter == null) {
-            //设备不支持蓝牙功能
-            Toast.makeText(this, "当前设备不支持蓝牙功能！", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // 打开设备的蓝牙功能
-        if (!mBluetoothAdapter.isEnabled()) {
-            boolean enable = mBluetoothAdapter.enable(); //返回值表示 是否成功打开了蓝牙设备
-            if (enable) {
-                Toast.makeText(this, "打开蓝牙功能成功！", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "打开蓝牙功能失败，请到'系统设置'中手动开启蓝牙功能！", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        }
-
-
-        ScanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(DeviceListActivity.this, "查找设备！", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        CancelButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
     }
-
-    public void onDestroy() {
-
-        super.onDestroy();
-        //解除注册
-        unregisterReceiver(mReceiver);
-        Log.e("destory","解除注册");
-    }
-
-    //定义广播
-    private BroadcastReceiver mReceiver=new BroadcastReceiver(){
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action=intent.getAction();
-            Log.e("ywq", action);
-            Toast.makeText(DeviceListActivity.this, "嗯好二手就你！", Toast.LENGTH_SHORT).show();
-
-            if(action.equals(BluetoothDevice.ACTION_FOUND))
-            {
-                BluetoothDevice device=intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if(device.getBondState()==BluetoothDevice.BOND_BONDED)
-                {
-
-                    // Add the name and address to an array adapter to show in a ListView
-                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                    mListView.setAdapter(mArrayAdapter);
-
-                }
-
-            }
-        }
-    };
 
     }
--- GRBL_APP/GRBLApplication/app/src/main/java/com/example/grblapplication/MainActivity.java
+++ GRBL_APP/GRBLApplication/app/src/main/java/com/example/grblapplication/MainActivity.java
@@ -1,36 +1,37 @@
 package com.example.grblapplication;
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
 import android.view.WindowManager;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.support.v7.widget.SwitchCompat;
-
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.bluetooth.BluetoothSocket;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import android.widget.Button;
-
-
-
+import android.util.Log;
+import android.view.View;
 import java.util.ArrayList;
 import java.util.Set;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
+    private BluetoothAdapter mBluetoothAdapter; // 本机蓝牙适配器对象
+    private static String TAG = "Bluetooth_State";
 
 
     @Override
@@ -40,17 +41,102 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();//隐藏标题栏
         setContentView(R.layout.activity_main);
 
-        Button FindEquipmentButton = (Button) findViewById(R.id.AddEquipment);
-        //添加蓝牙设备
-        FindEquipmentButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "添加设备", Toast.LENGTH_SHORT).show();
-                Intent intent =new Intent(MainActivity.this,DeviceListActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // 获得本机蓝牙适配器对象引用
+
+
+
+        if (mBluetoothAdapter == null)
+        {
+            toast("该设备不支持蓝牙功能！");
+            return;
+        }
+        int initialBTState = mBluetoothAdapter.getState();
+        printBTState(initialBTState); // 初始时蓝牙状态
+
+        InitialViews();
+
 
     }
+
+    private void InitialViews()
+    {
+        Button FindEquipmentButton = (Button) findViewById(R.id.AddEquipment);
+        Button XUp = (Button) findViewById(R.id.XUp);
+        Button XDown = (Button) findViewById(R.id.XDown);
+
+        Button YUp = (Button) findViewById(R.id.YUp);
+        Button YDown = (Button) findViewById(R.id.YDown);
+
+        FindEquipmentButton.setOnClickListener(this);
+        XUp.setOnClickListener(this);
+        XDown.setOnClickListener(this);
+        YUp.setOnClickListener(this);
+        YDown.setOnClickListener(this);
+    }
+
+
+
+
+    private void printBTState(int btState)
+    {
+        switch (btState)
+        {
+            case BluetoothAdapter.STATE_OFF:
+                toast("蓝牙状态:已关闭");
+                Log.v(TAG, "BT State ： BluetoothAdapter.STATE_OFF ###");
+                break;
+            case BluetoothAdapter.STATE_TURNING_OFF:
+                toast("蓝牙状态:正在关闭");
+                Log.v(TAG, "BT State :  BluetoothAdapter.STATE_TURNING_OFF ###");
+                break;
+            case BluetoothAdapter.STATE_TURNING_ON:
+                toast("蓝牙状态:正在打开");
+                Log.v(TAG, "BT State ：BluetoothAdapter.STATE_TURNING_ON ###");
+                break;
+            case BluetoothAdapter.STATE_ON:
+                toast("蓝牙状态:已打开");
+                Log.v(TAG, "BT State ：BluetoothAdapter.STATE_ON ###");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void toast(String str)
+    {
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+    }
+
+    private final int REQUEST_OPEN_BT_CODE = 1;
+    private final int REQUEST_DISCOVERY_BT_CODE = 2;
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        //真机调试时看不到LogCat，下面为解决方法
+        //在拨号界面输入 *#*#2846579#*#*
+        //记得重启哈
+        boolean wasBtOpened = mBluetoothAdapter.isEnabled(); // 是否已经打开
+        switch (v.getId())
+        {
+            case R.id.AddEquipment:
+                Log.e(TAG, " ## click btOpenBySystem ##");
+                if (!wasBtOpened) //未打开蓝牙，才需要打开蓝牙
+                {
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent, REQUEST_OPEN_BT_CODE);
+                }
+
+
+                break;
+
+
+
+        }
+    }
+
 
 }
 
